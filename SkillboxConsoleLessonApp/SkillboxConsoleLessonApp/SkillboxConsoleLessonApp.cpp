0ee8ad7a74f41841c60365b5d84aaa4efc36d5d0
Angel-senpai <daniil.murygin68@gmail.com>
2023-12-01 19:42:20 +0300

    fixes

--- SkillboxConsoleLessonApp/SkillboxConsoleLessonApp/SkillboxConsoleLessonApp.cpp
+++ SkillboxConsoleLessonApp/SkillboxConsoleLessonApp/SkillboxConsoleLessonApp.cpp
@@ -12,5 +12,5 @@ int main()
         std::cout << "Can't find characters in string";
         return 0; 
     }
-    std::cout << "Current string: " + text + "\n" << "Fist char: " << text[0] << "\nChar count: " << text.length() << "\nLast char: " << text[text.length() - 1];
+    std::cout << "Current string: " + text + "\n" << "First char: " << text[0] << "\nChar count: " << text.length() << "\nLast char: " << text[text.length() - 1];
 }
