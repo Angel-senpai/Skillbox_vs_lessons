fa3b06030e38f9d4ecf408fb41967fdabbd697bf
Angel-senpai <daniil.murygin68@gmail.com>
2023-12-01 16:44:15 +0300

    initial project
    13.4 practice

---
+++ SkillboxConsoleLessonApp/SkillboxConsoleLessonApp/Helpers.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// функция возвращает квадрат суммы двух чисел
+int sqrtSum(int x, int y) {
+	return (x + y) * (x + y);
+}
---
+++ SkillboxConsoleLessonApp/SkillboxConsoleLessonApp/SkillboxConsoleLessonApp.cpp
@@ -0,0 +1,10 @@
+﻿// SkillboxConsoleLessonApp.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{   
+    std::cout << sqrtSum(2, 2) << '\n';
+}
